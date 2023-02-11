23ef2c9894a5449a0eab8b121eb48a840f3d5c8d
Wesley Ashfield <lwashfield@gmail.com>
2023-02-10 19:23:21 -0500

    add db and models folders

---
+++ models/score.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Score extends Model {}
+
+Score.init(
+    {   
+    user_id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        autoIncrement: false
+      },
+    password_id: {
+        type: DataTypes.STRING
+      },
+    highscore: {
+        type: DataTypes.INTEGER
+      },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'score'
+    }
+);
+
+module.exports = Score;
