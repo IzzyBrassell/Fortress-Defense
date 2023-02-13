1973984c0c04b78465d069a6d45ae5a339e829eb
Israel Brassell <israel.brassell@gmail.com>
2023-02-13 16:37:50 -0500

    first route commited

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const _ = require('underscore')
 const path = require('path');
 const sequelize = require('./config/connection');
 
-const Book = require('./models/Book');
+// const Book = require('./models/Book');
 
 
 const app = express();
 const PORT = process.env.PORT || 5800;
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`now listening on port ${PORT}`)
 } )
+
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/game.html'))
+})
