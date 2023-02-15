cad201876dfb1edde817b4652a7a2331501b3513
Israel Brassell <israel.brassell@gmail.com>
2023-02-14 20:25:02 -0500

    bug fixes

--- Config/Connection.js
+++ Config/Connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.JAWDB_URL);
+const sequelize = new Sequelize(`mysql://mkfzp6v9u6y7cc9j:j72xgjmsguad3r2o@z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tdf20jsqjy1jqpx1`);
 
 module.exports = sequelize;
