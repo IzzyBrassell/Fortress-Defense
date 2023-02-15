c0afd9bb6a5b2beaa9f4e12eaf8faa79c0005007
Elmer Herrera <dcshoeco109790@yahoo.com>
2023-02-15 13:52:16 -0500

    scratching head

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+const Score = require('../../models/score');
+
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    try {
+      const newUser = req.body;
+      newScore.password = await bcrypt.hash(req.body.password, 10);
+      const ScoreData = await Score.create(newScore);
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  module.exports = router;
+  
